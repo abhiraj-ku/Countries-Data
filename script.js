54137fd0f295d411d1cc36236156b755a3e1d347
abhishek <abhirajabhi312@gmail.com>
2023-04-04 01:28:43 +0530

    added media Queries

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
-      console.log(country.flags.svg);
+      //   console.log(country.flags.svg);
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
       countryCard.href = `country.html?name=${country.name.common}`;
