b36a4c98dbcf536e8f992e57efe93dac23f6c647
abhishek <abhirajabhi312@gmail.com>
2023-04-04 23:46:46 +0530

    finished with countries page

--- country.js
+++ country.js
@@ -1,7 +1,65 @@
 const countryName = new URLSearchParams(location.search).get("name");
+const flagImg = document.querySelector(".country-details img");
+const countryNameH1 = document.querySelector("h1");
+const nativeName = document.querySelector(".native-name");
+const population = document.querySelector(".population");
+const region = document.querySelector(".region");
+const subRegion = document.querySelector(".sub-region");
+const capital = document.querySelector(".capital");
+const tld = document.querySelector(".tld");
+const language = document.querySelector(".language");
+const currency = document.querySelector(".currency");
+const borderCountries = document.querySelector(".border-countries");
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   .then((res) => res.json())
-  .then((data) => {
-    console.log(data[0]);
+  .then(([country]) => {
+    // console.log(country);
+    flagImg.src = country.flags.svg;
+    countryNameH1.innerText = country.name.common;
+
+    //native name
+    if (country.name.nativeName) {
+      nativeName.innerText = Object.values(country.name.nativeName)[0].common;
+    } else {
+      nativeName.innerText = country.name.common;
+    }
+    // population
+    population.innerText = country.population.toLocaleString("en-IN");
+    // region
+    region.innerText = country.region;
+    // subregion if empty make sure nothing is displayed
+    if (country.subregion) {
+      subRegion.innerText = country.subregion;
+    }
+    // capital if empty make sure nothing is displayed
+    if (country.capital) {
+      capital.innerText = country.capital;
+    }
+    //top level domain
+    tld.innerText = country.tld;
+    //currencies if many separate with comma
+    if (country.currencies) {
+      currency.innerText = Object.values(country.currencies)
+        .map((currency) => currency.name)
+        .join(",");
+    }
+    //languages if many separate with comma
+    if (country.languages) {
+      language.innerText = Object.values(country.languages).join(",");
+    }
+    if (country.borders) {
+      country.borders.forEach((border) => {
+        // console.log(border);
+        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+          .then((res) => res.json())
+          .then(([borderCountry]) => {
+            const borderCountryTag = document.createElement("a");
+            borderCountryTag.innerText = borderCountry.name.common;
+            borderCountryTag.href = `country.html?name=${borderCountry.name.common}`;
+            borderCountries.append(borderCountryTag);
+          });
+      });
+    }
   });
+// console.log();
