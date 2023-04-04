c4fcf4faab12045f4ab80bbc2593b39b96bdb398
abhishek <abhirajabhi312@gmail.com>
2023-04-05 00:47:12 +0530

    added search and filter buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countriesContainer = document.querySelector(".countries-container");
-
+const filterByRegion = document.querySelector(".search-filter-container");
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
@@ -24,3 +24,28 @@ fetch("https://restcountries.com/v3.1/all")
   });
 
 //   new URLSearchParams(location.search).get("name");
+
+filterByRegion.addEventListener("change", (e) => {
+  // console.log(e.target.value);
+  fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((country) => {
+        const countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `country.html?name=${country.name.common}`;
+        countryCard.innerHTML = `
+      <img src="${country.flags.svg}" alt="${country.name.common}" />
+      <div class="card-text">
+            <h3 class="card-title">${country.name.common}</h3>
+            <p><b> Population: </b> ${country.population.toLocaleString(
+              "en-IN"
+            )}</p>
+             <p><b>Region: </b>${country.region}</p>
+             <p><b>Capital: </b>${country.capital}</p>
+      </div>
+      `;
+        countriesContainer.append(countryCard);
+      });
+    });
+});
