6d1ddbcd52b001a3d1333d0015ba61fa096ac1f6
abhishek <abhirajabhi312@gmail.com>
2023-04-05 08:47:21 +0530

    filter option implementation  done

--- script.js
+++ script.js
@@ -2,39 +2,25 @@ const countriesContainer = document.querySelector(".countries-container");
 const filterByRegion = document.querySelector(".search-filter-container");
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
-  .then((data) => {
-    data.forEach((country) => {
-      //   console.log(country.flags.svg);
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `country.html?name=${country.name.common}`;
-      countryCard.innerHTML = `
-      <img src="${country.flags.svg}" alt="${country.name.common}" />
-      <div class="card-text">
-            <h3 class="card-title">${country.name.common}</h3>
-            <p><b> Population: </b> ${country.population.toLocaleString(
-              "en-IN"
-            )}</p>
-             <p><b>Region: </b>${country.region}</p>
-             <p><b>Capital: </b>${country.capital}</p>
-      </div>
-      `;
-      countriesContainer.append(countryCard);
-    });
-  });
+  .then(renderCountries);
 
 //   new URLSearchParams(location.search).get("name");
 
 filterByRegion.addEventListener("change", (e) => {
-  // console.log(e.target.value);
+  console.log(e.target.value);
   fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
     .then((res) => res.json())
-    .then((data) => {
-      data.forEach((country) => {
-        const countryCard = document.createElement("a");
-        countryCard.classList.add("country-card");
-        countryCard.href = `country.html?name=${country.name.common}`;
-        countryCard.innerHTML = `
+    .then(renderCountries);
+});
+
+function renderCountries(data) {
+  countriesContainer.innerHTML = "";
+  data.forEach((country) => {
+    console.log(country);
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `country.html?name=${country.name.common}`;
+    countryCard.innerHTML = `
       <img src="${country.flags.svg}" alt="${country.name.common}" />
       <div class="card-text">
             <h3 class="card-title">${country.name.common}</h3>
@@ -45,7 +31,6 @@ filterByRegion.addEventListener("change", (e) => {
              <p><b>Capital: </b>${country.capital}</p>
       </div>
       `;
-        countriesContainer.append(countryCard);
-      });
-    });
-});
+    countriesContainer.append(countryCard);
+  });
+}
